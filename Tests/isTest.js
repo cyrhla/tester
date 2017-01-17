17a0bad5b1e356faebc168548fd32072ac5f75aa
kpietru <kpietru@users.noreply.github.com>
2017-01-17 20:29:21 +0100

    Create isTest.js

---
+++ Tests/isTest.js
@@ -0,0 +1,33 @@
+/**
+ * @package @cyrhla/tester
+ * @license MIT
+ * @copyright Krystian Pietruszka <kpietru@cyrhla.com>
+ * @see http://www.cyrhla.com
+ */
+
+'use strict'
+
+const Tester = require('@cyrhla/tester/Tester')
+const is = require('../is')
+
+/**
+ * isTest
+ *
+ * @author Krystian Pietruszka <kpietru@cyrhla.com>
+ */
+module.exports = class isTest extends Tester
+{
+    testIsReturnsBoolean()
+    {
+        this.assertSame(true, is(0, 'string', '*'))
+        this.assertSame(true, is('', 'mixed'))
+
+        this.assertSame(true, is(123, 'number'))
+        this.assertSame(true, is('', 'boolean', 'array', 'string'))
+        this.assertSame(true, is(Symbol('foo'), 'boolean', 'array', 'string', 'symbol'))
+        this.assertSame(true, is(new Object(), 'function', 'object', 'string'))
+        this.assertSame(false, is(Object, 'boolean', 'string'))
+        this.assertSame(false, is([], 'boolean', 'string'))
+        this.assertSame(false, is(/^/, 'function', 'object'))
+    }
+}
