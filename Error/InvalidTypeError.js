a0d34b6b589f75883b8a82c74be066e6d371448d
kpietru <kpietru@users.noreply.github.com>
2017-01-29 16:44:35 +0100

    Create InvalidTypeError.js

---
+++ Error/InvalidTypeError.js
@@ -0,0 +1,17 @@
+/**
+ * @package @cyrhla/tester
+ * @license MIT
+ * @copyright Krystian Pietruszka <kpietru@cyrhla.com>
+ * @see http://www.cyrhla.com
+ */
+
+'use strict'
+
+/**
+ * The InvalidTypeError thrown when invalid type.
+ *
+ * @author Krystian Pietruszka <kpietru@cyrhla.com>
+ */
+module.exports = class InvalidTypeError extends TypeError
+{
+}
