5e39afadf3d6f2470a59057a4c8f223810bc51c8
kpietru <kpietru@users.noreply.github.com>
2017-02-03 16:24:42 +0100

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,6 +1,6 @@
 'use strict'
 
+new (require('@cyrhla/tester/Tests/Mocker/MockerTest'))()
 new (require('@cyrhla/tester/Tests/TesterTest'))()
 new (require('@cyrhla/tester/Tests/isTest'))()
 new (require('@cyrhla/tester/Tests/validTest'))()
-new (require('@cyrhla/tester/Tests/Mocker/MockerTest'))()
