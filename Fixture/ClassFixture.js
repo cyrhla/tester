9b51dc3a80a48d65efa8a7d5cf113c641cbb7590
kpietru <kpietru@users.noreply.github.com>
2017-03-26 11:56:00 +0200

    Create ClassFixture.js

---
+++ Fixture/ClassFixture.js
@@ -0,0 +1,62 @@
+/**
+ * @package @cyrhla/tester
+ * @license MIT
+ * @copyright Krystian Pietruszka <kpietru@cyrhla.com>
+ * @see http://www.cyrhla.com
+ */
+
+'use strict'
+
+/**
+ * The class for tests.
+ *
+ * @author Krystian Pietruszka <kpietru@cyrhla.com>
+ */
+module.exports = class classFixture
+{
+    /**
+     * Initializes this class with the given options.
+     *
+     * @param mixed foo
+     * @param mixed bar
+     * @param mixed baz
+     */
+    constructor(foo, bar, baz)
+    {
+        /** @type mixed */
+        this.foo = foo
+
+        /** @type mixed */
+        this.bar = bar
+
+        /** @type mixed */
+        this.baz = baz
+    }
+
+    /**
+     * Sets the key-value pair.
+     *
+     * @param mixed key
+     * @param mixed value
+     *
+     * @return self The invoked object.
+     */
+    set(key, value)
+    {
+        this[key] = value
+
+        return this
+    }
+
+    /**
+     * Gets the value in the key-value pair.
+     *
+     * @param mixed key
+     *
+     * @return mixed
+     */
+    get(key)
+    {
+        return this[key]
+    }
+}
