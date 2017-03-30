bb06bd35464c7bf224a6bed2b547030d900b35a2
kpietru <kpietru@users.noreply.github.com>
2017-03-30 17:54:07 +0200

    Update type.cc

--- type.cc
+++ type.cc
@@ -15,32 +15,30 @@ void Type(const FunctionCallbackInfo<Value>& args) {
     }
 
     if (!args[0]->IsObject()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "object");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "object"));
     } else if (!args[0]->IsBoolean()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "boolean");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "boolean"));
     } else if (!args[0]->IsArray()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "array");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "array"));
     } else if (!args[0]->IsString()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "string");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "string"));
     } else if (!args[0]->IsNumber()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "number");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "number"));
     } else if (!args[0]->IsFunction()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "function");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "function"));
     } else if (!args[0]->IsNull()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "null");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "null"));
     } else if (!args[0]->IsUndefined()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "undefined");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "undefined"));
     } else if (!args[0]->IsSymbol()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "symbol");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "symbol"));
     } else if (!args[0]->IsRegExp()) {
-        Local<String> ta = String::NewFromUtf8(isolate, "regexp");
+        args.GetReturnValue().Set(String::NewFromUtf8(isolate, "regexp"));
     }
-
-    args.GetReturnValue().Set(t); 
-} 
+}
 
 void Init(Local <Object> exports, Local <Object> module) { 
     NODE_SET_METHOD(module, "exports", Type); 
-}
+} 
 
 NODE_MODULE(type, Init)
