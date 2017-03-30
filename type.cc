90a1b4a8af980788e5b7bb2edc8e7ed1eb5b2326
kpietru <kpietru@users.noreply.github.com>
2017-03-30 21:43:19 +0200

    Update type.cc

--- type.cc
+++ type.cc
@@ -21,11 +21,6 @@ void Type(const FunctionCallbackInfo<Value>& args) {
 
     for (int i = 0; i < args.Length(); i++) {
         Local<Value> argN = args[i];
-        if  (
-            argN == 'mixed'
-        ) {
-        
-        }
     }
 
     if (args[0]->IsBoolean()) {
